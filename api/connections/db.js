49494c543dacb40d15231bbd5210aa51f9e90ed0
MaddoxDevelopment <jm@madev.me>
2019-01-17 22:58:35 -0600

    database health check

--- api/connections/db.js
+++ api/connections/db.js
@@ -1,30 +1,36 @@
 const Database = (() => {
     let instance;
 
-    function createInstance() {
-        const {Client} = require('pg');
-        const instance = new Client({
-            user: process.env.postgres_user,
-            host: process.env.postgres_host,
-            database: process.env.postgres_database,
-            password: process.env.postgres_password,
-            port: process.env.postgres_port,
-            ssl: true
+    async function createInstance() {
+        return new Promise((resolve, reject) => {
+            const {Client} = require('pg');
+            const instance = new Client({
+                user: process.env.postgres_user,
+                host: process.env.postgres_host,
+                database: process.env.postgres_database,
+                password: process.env.postgres_password,
+                port: process.env.postgres_port,
+                ssl: true
+            });
+            instance.connect((err) => {
+                err ? reject(err) : resolve(instance);
+            });
         });
-        console.log("Connecting to database.");
-        instance.connect();
-        return instance;
     }
 
     return {
+        isAlive : async function() {
+          const instance = await createInstance();
+          return instance != null;
+        },
         cleanup: function () {
             if (instance != null) {
                 instance.end();
             }
         },
-        getInstance: function () {
+        getInstance: async function () {
             if (!instance) {
-                instance = createInstance();
+                instance = await createInstance();
             }
             return instance;
         }
--- api/handlers/healthHandler.js
+++ api/handlers/healthHandler.js
@@ -1,7 +1,18 @@
 const Redis = require("../connections/redis");
+const Database = require("../connections/db");
 const {handle, response} = require("./util/lambda");
 
 handle('redis', async (event, context) => {
   const alive = await Redis.isAlive();
   return response(alive ? 200 : 503, {alive})
+}, module);
+
+handle('database', async (event, context) => {
+  try {
+    const alive = await Database.isAlive();
+    return response(200, {alive})
+  } catch (e) {
+    console.log(e);
+    return response(503, {alive : false})
+  }
 }, module);
